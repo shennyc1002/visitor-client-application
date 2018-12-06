a13f5f49a959253f29a05e199fcecfcfd86000c8
shennychacko <shennychacko80@gmail.com>
2018-12-05 20:14:49 -0700

    initial checkin

---
+++ src/main/java/com/example/microservices/visitorclientapplication/VisitorClientApplication.java
@@ -0,0 +1,12 @@
+package com.example.microservices.visitorclientapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisitorClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisitorClientApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/microservices/visitorclientapplication/VisitorClientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.microservices.visitorclientapplication;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VisitorClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
